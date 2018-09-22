e1a97c9080fc839a33460cf22ea1c20227603e9f
Forestsong <forestsong@Forestsongs-MacBook-Pro.local>
2018-09-22 14:00:07 -0700

    Test

--- app/src/main/java/com/example/sijiagao/whatsfordinner/controller/MainActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/controller/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //test
+
         ImageButton newDishImageBtn = findViewById(R.id.newDishImageBtn);
         ImageButton recipeImageBtn = findViewById(R.id.recipeImageBtn);
         ImageButton mealImageBtn = findViewById(R.id.mealImageBtn);
