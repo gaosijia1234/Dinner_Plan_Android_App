ee97722d427e1ef0c5562807ac7783917cfb0aa5
Rui JIN <ruijin@Ruis-iMac-Pro-2.local>
2018-09-27 17:46:15 -0700

    FragmentGroccey class setup 2

--- app/src/main/java/com/example/sijiagao/whatsfordinner/activity/GroceryActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/activity/GroceryActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sijiagao.whatsfordinner.R;
+import com.example.sijiagao.whatsfordinner.fragment.FragmentGrocey;
 
 public class GroceryActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class GroceryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery);
+
+        FragmentGrocey fragment = new FragmentGrocey();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment1,fragment)
+                .commit();
     }
 }
