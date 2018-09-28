acd761c7a9857d73a0d6f29ad95693c41bacb747
dashabishiwo <jessie_gao@yahoo.com>
2018-09-28 13:05:05 -0700

    grab data from ingredient in newDishView

--- app/src/main/java/com/example/sijiagao/whatsfordinner/activity/NewdishActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/activity/NewdishActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -56,14 +57,28 @@ public class NewdishActivity extends AppCompatActivity {
             }
         });
 
-        // **************************************** for drop down menu *****************************************************
+        // **************************************** drop down menu variables *****************************************************
         // name of the ingre
         TextView ingredientNameTV1 = findViewById(R.id.ingredientNameTV1);
         // quantity of the ingre
         TextView ingredientTVQuantity1 = findViewById(R.id.ingredientQuantityTV1);
-        // unit of the ingre
-        TextView ingredientUnitTV1 = findViewById(R.id.ingredientUnitTV1);
+        // to make sure that only one decimal point can be entered, e.g. 1.2 is ok, 1..2 is not ok
+        ingredientTVQuantity1.setKeyListener(DigitsKeyListener.getInstance(true,true));
 
+        //2
+        TextView ingredientNameTV2 = findViewById(R.id.ingredientNameTV2);
+        TextView ingredientTVQuantity2 = findViewById(R.id.ingredientQuantityTV2);
+        ingredientTVQuantity2.setKeyListener(DigitsKeyListener.getInstance(true,true));
+
+        //3
+        TextView ingredientNameTV3 = findViewById(R.id.ingredientNameTV3);
+        TextView ingredientTVQuantity3 = findViewById(R.id.ingredientQuantityTV3);
+        ingredientTVQuantity3.setKeyListener(DigitsKeyListener.getInstance(true,true));
+
+        // *************** change the TextViews to Strings and doubles **************************
+        // unit of the ingre
+        // 1
+        TextView ingredientUnitTV1 = findViewById(R.id.ingredientUnitTV1);
         String ingredientName1 = ingredientNameTV1.toString();
         try {
             double ingredientQty1 = Double.parseDouble(ingredientTVQuantity1.toString());
@@ -72,6 +87,25 @@ public class NewdishActivity extends AppCompatActivity {
         }
         String ingredientUnit1 = ingredientUnitTV1.toString();
 
+        // 2
+        TextView ingredientUnitTV2 = findViewById(R.id.ingredientUnitTV2);
+        String ingredientName2 = ingredientNameTV2.toString();
+        try {
+            double ingredientQty2 = Double.parseDouble(ingredientTVQuantity2.toString());
+        } catch (NumberFormatException e) {
+        }
+        String ingredientUnit2 = ingredientUnitTV2.toString();
+
+        //3
+        TextView ingredientUnitTV3 = findViewById(R.id.ingredientUnitTV3);
+        String ingredientName3 = ingredientNameTV3.toString();
+        try {
+            double ingredientQty3 = Double.parseDouble(ingredientTVQuantity3.toString());
+        } catch (NumberFormatException e) {
+        }
+        String ingredientUnit3 = ingredientUnitTV3.toString();
+
+        // ***************************************** Drop down menu function **************************************************
 
         String[] myIngredient = { "tomato","salt","pepper","tst"};
 
