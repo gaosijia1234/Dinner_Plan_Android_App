5033222fa0966806848df4c81ccd841bf015d118
Rui JIN <ruijin@Ruis-iMac-Pro-2.local>
2018-09-30 13:51:48 -0700

    set up ListFragment and DetailFragment for RecipeActivity

--- app/src/main/java/com/example/sijiagao/whatsfordinner/activity/RecipeActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/activity/RecipeActivity.java
@@ -7,10 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ResourceCursorAdapter;
 
 import com.example.sijiagao.whatsfordinner.R;
-import com.example.sijiagao.whatsfordinner.fragment.RecipeListFragment;
+import com.example.sijiagao.whatsfordinner.fragment.RecipeDetailFragment;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -22,18 +21,19 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-       Resources resource = getResources();
-       recipeListView = findViewById(R.id.recipeListView);
-       recipes = getResources().getStringArray(R.array.recipes);
+//       Resources resource = getResources();
+//       recipeListView = findViewById(R.id.recipeListView);
+//       recipes = getResources().getStringArray(R.array.recipes);
+//
+//        // now we need to merge recipeListView(layout) and recipes(content) together
+//        recipeListView.setAdapter(new ArrayAdapter<String>(this, R.layout.recipe_listview_detail, recipes));
 
-        // now we need to merge recipeListView(layout) and recipes(content) together
-        recipeListView.setAdapter(new ArrayAdapter<String>(this, R.layout.recipe_listview_detail, recipes));
 
-
-        RecipeListFragment fragment = RecipeListFragment.newInstance("Pass message from GroceryActivity");
+       // RecipeDetailFragment fragment = RecipeDetailFragment.newInstance("Pass message from GroceryActivity");
+        RecipeDetailFragment fragment = new RecipeDetailFragment();
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fragment1,fragment)
+                .add(R.id.detailFragment,fragment)
                 .commit();
 
         //DatabaseHelper helper = DatabaseHelper.getInstance(this);
@@ -42,23 +42,23 @@ public class RecipeActivity extends AppCompatActivity {
     }
 
 
-    public void addClickHandler(View view) {
-        RecipeListFragment fragment = RecipeListFragment.newInstance("Pass message from GroceryActivity");
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fragment1,fragment)
-                .commit();
-    }
-
-
-    public void removeClickHandler(View view) {
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment1);
-        if ( fragment != null ) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .remove(fragment)
-                    .commit();
-        }
-    }
+//    public void addClickHandler(View view) {
+//        RecipeDetailFragment fragment = RecipeDetailFragment.newInstance("Pass message from GroceryActivity");
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.fragment1,fragment)
+//                .commit();
+//    }
+//
+//
+//    public void removeClickHandler(View view) {
+//        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment1);
+//        if ( fragment != null ) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .remove(fragment)
+//                    .commit();
+//        }
+//    }
 
 }
--- app/src/main/java/com/example/sijiagao/whatsfordinner/fragment/RecipeListFragment.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/fragment/RecipeListFragment.java
@@ -1,15 +1,11 @@
 package com.example.sijiagao.whatsfordinner.fragment;
 
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.sijiagao.whatsfordinner.R;
 
@@ -18,82 +14,17 @@ import com.example.sijiagao.whatsfordinner.R;
  */
 public class RecipeListFragment extends Fragment {
 
-      public static final String TAG = "LifecycleEvents";
-      public static final String MESSAGE_KEY="message_key";
-
-
 
     public RecipeListFragment() {
         // Required empty public constructor
     }
 
-    public static RecipeListFragment newInstance(String message) {
-
-        Bundle args = new Bundle();
-        args.putString(RecipeListFragment.MESSAGE_KEY, message);
-        RecipeListFragment fragment = new RecipeListFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Log.i(TAG,"onAttach");
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG,"onCreate");
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.i(TAG,"onCreateView");
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_recipe, container, false);
-
-        Bundle arguments = getArguments();
-
-        if (arguments != null) {
-            String message = arguments.getString(MESSAGE_KEY);
-            TextView tvMessage = (TextView) view.findViewById(R.id.message);
-            tvMessage.setText(message);
-        }
-
-        return view;
+        return inflater.inflate(R.layout.fragment_recipe_list, container, false);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i(TAG,"onResume");
-
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.i(TAG,"onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.i(TAG,"onStop");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG,"onDestory");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.i(TAG,"onDetach");
-    }
 }
