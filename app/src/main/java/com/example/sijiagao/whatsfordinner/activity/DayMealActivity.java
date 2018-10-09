7fce1a764ec657900787c6a86f7fd14ce5d0c2e3
RuiJin <rui.jin@sjsu.edu>
2018-10-09 09:32:25 -0700

    meal almost done still need talk with backend

--- app/src/main/java/com/example/sijiagao/whatsfordinner/activity/DayMealActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/activity/DayMealActivity.java
@@ -21,8 +21,13 @@ public class DayMealActivity extends AppCompatActivity {
     private Spinner breakfast_sp;
     private Spinner lunch_sp;
     private Spinner dinner_sp;
-    List<String> igList ;
+    private int breakfast_position;
+    private int lunch_position;
+    private int dinner_position;
+    List<String> igList = new ArrayList<String>();
     private String today;
+    DatabaseHelper db = DatabaseHelper.getInstance(this);
+    private ArrayAdapter<String> dataAdapter;
 
 
     public static final String EATINGOUT = "eatingout";
@@ -30,7 +35,6 @@ public class DayMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_meal);
-
         setUp();
         linsterSetUp();
 
@@ -38,45 +42,40 @@ public class DayMealActivity extends AppCompatActivity {
     }
 
     public void setUp(){
-        DatabaseHelper db = DatabaseHelper.getInstance(this);
-
         Log.i("ggggg","yy");
         TextView day = findViewById(R.id.daymealLabelTV);
         today = getIntent().getStringExtra("day");
         day.setText(today);
 
-//        Log.i("ggggg",db.getMealPlanByDay(today).get(0));
-        igList = new ArrayList<String>();
-
-        igList.add("Automobile");
-        igList.add("Business Services");
-        igList.add("Computers");
-        igList.add("Education");
-        igList.add("Personal");
-        igList.add("Travel");
-       // Log.i("ggggg",db.getMealPlanByDay(today).get(0));
+        TreeMap<String,Integer> mealList=  db.getAllMeal();
 
 
-//        igList.add(db.getMealPlanByDay(today).get(0));
+        if (!db.getAllMeal().isEmpty())
+        { igList = new ArrayList<String>(db.getAllMeal().keySet());}
+//
 
         breakfast_sp = (Spinner) findViewById(R.id.breakfast_sp);
         lunch_sp     = (Spinner) findViewById(R.id.lunch_sp);
         dinner_sp    = (Spinner) findViewById(R.id.dinner_sp);
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, igList);
+        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, igList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         breakfast_sp.setAdapter(dataAdapter);
         lunch_sp.setAdapter(dataAdapter);
         dinner_sp.setAdapter(dataAdapter);
 
+
+
     }
 
+    public void  linsterSetUp() {
 
-    public void linsterSetUp() {
         breakfast_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("ggggg",igList.get(position));
+                breakfast_position=position;
 
             }
 
@@ -89,7 +88,7 @@ public class DayMealActivity extends AppCompatActivity {
         lunch_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                lunch_position=position;
             }
 
             @Override
@@ -102,7 +101,7 @@ public class DayMealActivity extends AppCompatActivity {
         dinner_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                dinner_position=position;
             }
 
             @Override
@@ -115,17 +114,26 @@ public class DayMealActivity extends AppCompatActivity {
 
 
     public void breakfastDoneBtnClick(View view){
+        db.assignRecipeToPlanSlot(today,"Breakfast",igList.get(breakfast_position));
+
+
+        igList = new ArrayList<String>(db.getAllMeal().keySet());
+        dataAdapter.notifyDataSetChanged();
+        dataAdapter.clear();
+        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, igList);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        breakfast_sp.setAdapter(dataAdapter);
 
     }
 
     public void lunchDoneBtnClick(View view){
 
-        // logic same as breakfast
+        db.assignRecipeToPlanSlot(today,"Lunch",igList.get(lunch_position));
     }
 
     public void dinnerDoneBtnClick(View view){
 
-        // logic same as breakfast
+        db.assignRecipeToPlanSlot(today,"Dinner",igList.get(dinner_position));
 
     }
 
@@ -145,9 +153,11 @@ public class DayMealActivity extends AppCompatActivity {
 
     public void dayMealDoneBtnClick(View view){
         // last line: go back to MealActivity
+
         Intent i = new Intent(this, MealActivity.class);
         startActivity(i);
     }
 
 
 }
+
