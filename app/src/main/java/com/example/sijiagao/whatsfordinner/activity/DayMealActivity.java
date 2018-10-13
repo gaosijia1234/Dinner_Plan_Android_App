5ef5164dbdc3fa5fe8190783fc31066027e64b78
Charlene Jiang <jiangxiaoyu1996@gmail.com>
2018-10-12 19:02:05 -0700

    Fix meal plan dropdown menu selection no default setup bug

--- app/src/main/java/com/example/sijiagao/whatsfordinner/activity/DayMealActivity.java
+++ app/src/main/java/com/example/sijiagao/whatsfordinner/activity/DayMealActivity.java
@@ -1,14 +1,17 @@
 package com.example.sijiagao.whatsfordinner.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sijiagao.whatsfordinner.R;
 import com.example.sijiagao.whatsfordinner.database.DatabaseHelper;
@@ -24,140 +27,192 @@ public class DayMealActivity extends AppCompatActivity {
     private int breakfast_position;
     private int lunch_position;
     private int dinner_position;
-    List<String> igList = new ArrayList<String>();
+    ArrayList<String> mealList;
     private String today;
-    DatabaseHelper db = DatabaseHelper.getInstance(this);
-    private ArrayAdapter<String> dataAdapter;
-
+    private ArrayAdapter<String> bfAdapter;
+    private ArrayAdapter<String> luAdapter;
+    private ArrayAdapter<String> diAdapter;
 
     public static final String EATINGOUT = "eatingout";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_meal);
-        setUp();
-        linsterSetUp();
 
-
-    }
-
-    public void setUp(){
-        Log.i("ggggg","yy");
-        TextView day = findViewById(R.id.daymealLabelTV);
-        today = getIntent().getStringExtra("day");
-        day.setText(today);
-
-        TreeMap<String,Integer> mealList=  db.getAllMeal();
-
-
-        if (!db.getAllMeal().isEmpty())
-        { igList = new ArrayList<String>(db.getAllMeal().keySet());}
-//
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
 
         breakfast_sp = (Spinner) findViewById(R.id.breakfast_sp);
         lunch_sp     = (Spinner) findViewById(R.id.lunch_sp);
         dinner_sp    = (Spinner) findViewById(R.id.dinner_sp);
+        breakfast_position = 0;
+        lunch_position = 0;
+        dinner_position = 0;
+        today = getIntent().getStringExtra("day");
+        mealList = new ArrayList<>(db.getAllMeal().keySet());
 
-        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, igList);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        TextView day = findViewById(R.id.daymealLabelTV);
+        day.setText(today);
 
-        breakfast_sp.setAdapter(dataAdapter);
-        lunch_sp.setAdapter(dataAdapter);
-        dinner_sp.setAdapter(dataAdapter);
-
-
-
+        setUp();
+        listenerSetup();
     }
 
-    public void  linsterSetUp() {
+    public void setUp(){
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
+
+        List<String> bfList = (ArrayList<String>)this.mealList.clone();
+        bfList.add(0, db.getMealPlanByDay(today).get(0));
+        bfAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, bfList){
+            @Override
+            public boolean isEnabled(int position){
+                return position != 0;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    tv.setTextColor(Color.GRAY);
+                }else{
+                    tv.setTextColor(Color.BLACK);
+                }
+                    return view;
+            }
+        };
+        bfAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        breakfast_sp.setAdapter(bfAdapter);
+
+        List<String> luList = (ArrayList<String>)this.mealList.clone();
+        luList.add(0, db.getMealPlanByDay(today).get(1));
+        luAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, luList){
+            @Override
+            public boolean isEnabled(int position){
+                return position != 0;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    tv.setTextColor(Color.GRAY);
+                }else{
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        luAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lunch_sp.setAdapter(luAdapter);
+
+        List<String> diList = (ArrayList<String>)this.mealList.clone();
+        diList.add(0, db.getMealPlanByDay(today).get(2));
+        diAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, diList){
+            @Override
+            public boolean isEnabled(int position){
+                return position != 0;
+            }
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getDropDownView(position, convertView, parent);
+                    TextView tv = (TextView) view;
+                    if(position == 0){
+                        tv.setTextColor(Color.GRAY);
+                    }else{
+                        tv.setTextColor(Color.BLACK);
+                    }
+                    return view;
+                }
+            };
+            diAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dinner_sp.setAdapter(diAdapter);
+    }
+
+    public void listenerSetup() {
 
         breakfast_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("ggggg",igList.get(position));
-                breakfast_position=position;
-
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    breakfast_position = position;
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
 
         lunch_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                lunch_position=position;
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    lunch_position = position;
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
-
 
         dinner_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                dinner_position=position;
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    dinner_position = position;
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
     }
 
-
-
     public void breakfastDoneBtnClick(View view){
-        db.assignRecipeToPlanSlot(today,"Breakfast",igList.get(breakfast_position));
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
+        if(breakfast_position > 0){
 
-
-        igList = new ArrayList<String>(db.getAllMeal().keySet());
-        dataAdapter.notifyDataSetChanged();
-        dataAdapter.clear();
-        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, igList);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        breakfast_sp.setAdapter(dataAdapter);
-
+            db.assignRecipeToPlanSlot(today,"Breakfast", mealList.get(breakfast_position - 1));
+        }
+        bfAdapter.clear();
+        setUp();
     }
 
     public void lunchDoneBtnClick(View view){
-
-        db.assignRecipeToPlanSlot(today,"Lunch",igList.get(lunch_position));
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
+        if(lunch_position > 0){
+            db.assignRecipeToPlanSlot(today,"Lunch", mealList.get(lunch_position - 1));
+        }
+        luAdapter.clear();
+        setUp();
     }
 
     public void dinnerDoneBtnClick(View view){
-
-        db.assignRecipeToPlanSlot(today,"Dinner",igList.get(dinner_position));
-
-    }
-
-
-
-    public String updateMealChoice(String input){
-
-
-        return "updateMealChoice method is called";
-    }
-
-    public String firstTimeMealChoice(String input){
-
-
-        return "firstTimeMealChoice method is called";
+        DatabaseHelper db = DatabaseHelper.getInstance(this);
+        if(dinner_position > 0){
+            db.assignRecipeToPlanSlot(today,"Dinner", mealList.get(dinner_position - 1));
+        }
+        diAdapter.clear();
+        setUp();
     }
 
     public void dayMealDoneBtnClick(View view){
-        // last line: go back to MealActivity
-
         Intent i = new Intent(this, MealActivity.class);
         startActivity(i);
     }
-
 
 }
 
